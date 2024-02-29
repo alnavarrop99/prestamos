25a33e8998c7e2ff090197e7ac91d26b3a56c838
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-29 16:43:21 -0500

    feat: add radio-group comp

---
+++ src/components/ui/radio-group.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { RadioGroup, RadioGroupItem } from './radio-group'
+import { Label } from './label'
+
+function Basic() {
+  return (
+    <RadioGroup defaultValue="comfortable">
+      <div className="flex items-center space-x-2">
+        <RadioGroupItem value="default" id="r1" />
+        <Label htmlFor="r1">Default</Label>
+      </div>
+      <div className="flex items-center space-x-2">
+        <RadioGroupItem value="comfortable" id="r2" />
+        <Label htmlFor="r2">Comfortable</Label>
+      </div>
+      <div className="flex items-center space-x-2">
+        <RadioGroupItem value="compact" id="r3" />
+        <Label htmlFor="r3">Compact</Label>
+      </div>
+    </RadioGroup>
+  )
+}
+
+const meta: Meta<React.ComponentProps<typeof RadioGroup>> = {
+  title: '@components/radio-group',
+  component: RadioGroup,
+}
+export default meta
+
+export const _Basic: StoryObj<React.ComponentProps<typeof Basic>> = {
+  name: 'Basic',
+  args: {},
+  render: Basic,
+}
