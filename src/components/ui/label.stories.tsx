dc91f8fbdc37399560a8dadfe00a7169265c6ae0
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-29 15:01:28 -0500

    feat: add label comp

---
+++ src/components/ui/label.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Label } from './label'
+import { Checkbox } from './checkbox'
+
+function Basic({ label }: { label: string }) {
+  return (
+    <Label className="inline-flex items-center gap-1">
+      <Checkbox /> {label}
+    </Label>
+  )
+}
+
+const meta: Meta<React.ComponentProps<typeof Label>> = {
+  title: '@components/label',
+  component: Label,
+}
+export default meta
+
+export const Primary: StoryObj<React.ComponentProps<typeof Basic>> = {
+  name: 'Basic',
+  args: {
+    label: 'Label',
+  },
+  render: Basic,
+}
