90ec2551c3a3d8526b855c77875b2dfaf0e2f0cd
alnavarrop99 <anavarro.develop99@gmail.com>
2024-03-01 04:38:49 -0500

    feat: create _layout to root route

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function subscribe(
+  eventName: string,
+  listener?: (param: unknown) => void
+) {
+  document.addEventListener(eventName, listener ?? (() => {}))
+}
+
+export function unsubscribe(
+  eventName: string,
+  listener?: (param: unknown) => void
+) {
+  document.removeEventListener(eventName, listener ?? (() => {}))
+}
+
+export function publish(eventName: string, data?: Record<string, unknown>) {
+  const event = new CustomEvent(eventName, { detail: data })
+  document.dispatchEvent(event)
+}
+
+export default {
+  subscribe,
+  unsubscribe,
+  publish,
+}
--- src/pages/_layout/-nav.stories.tsx
+++ src/pages/_layout/-nav.stories.tsx
@@ -5,22 +5,31 @@ import { Client } from './client'
 import { Credit } from './credit'
 import { Home } from './index'
 import { Navigation } from '../_layout.tsx'
+import { Fragment } from 'react'
 
 function Layout(Story: StoryFn) {
   return (
     <Navigation>
-      {' '}
-      <Story />{' '}
+      <Story />
     </Navigation>
   )
 }
 
+function Router(Story: StoryFn) {
+  return $.customRenderStorie(() => <Story />)
+}
+
 const meta: Meta = {
   title: '@pages/nav',
-  component: $.customRenderStorie,
-  decorators: [Layout],
+  component: Fragment,
+  decorators: [Layout, Router],
 }
 export default meta
+
+export const _Home: StoryObj = {
+  name: '/',
+  render: Home,
+}
 
 export const _User: StoryObj = {
   name: '/user',
@@ -35,9 +44,4 @@ export const _Client: StoryObj = {
 export const _Credit: StoryObj = {
   name: '/credit',
   render: Credit,
-}
-
-export const _Home: StoryObj = {
-  name: '/home',
-  render: Home,
 }
