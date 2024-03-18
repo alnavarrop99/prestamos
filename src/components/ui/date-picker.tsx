c8ab73143586bfd7243f582be7c59f45b23d7251
alnavarrop99 <anavarro.develop99@gmail.com>
2024-03-18 03:38:33 -0400

    fix: set DatePicker wit reference and input hidden

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -1,5 +1,4 @@
 import { Calendar as CalendarIcon } from 'lucide-react'
-
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
@@ -12,13 +11,17 @@ import { ComponentProps, forwardRef, useState } from 'react'
 import { addDays, format } from 'date-fns'
 import { DateRange } from 'react-day-picker'
 
-interface TDatePicker extends ComponentProps< typeof Button > {
+interface TDatePicker extends ComponentProps<typeof Button> {
   label?: string
   range?: boolean
   date?: Date
+  required?: boolean
 }
 
-export const DatePicker = forwardRef<HTMLButtonElement, TDatePicker>(function({ label, range, date: _date, ...props }, ref) {
+export const DatePicker = forwardRef<HTMLButtonElement, TDatePicker>(function (
+  { label, range, date: _date, name, required, ...props },
+  ref
+) {
   const [date, setDate] = useState<Date | undefined>(_date)
   const [dateRange, setDateRange] = useState<DateRange | undefined>({
     from: new Date(2022, 0, 20),
@@ -32,23 +35,24 @@ export const DatePicker = forwardRef<HTMLButtonElement, TDatePicker>(function({ 
           <PopoverTrigger asChild>
             <Button
               variant={'outline'}
-              { ...props }
+              {...props}
               ref={ref}
               id="date"
               className={cn(
                 'w-full justify-start text-left font-normal',
-                !dateRange && 'text-muted-foreground', props.className
+                !dateRange && 'text-muted-foreground',
+                props.className
               )}
             >
               <CalendarIcon className="mr-2 h-4 w-4" />
               {dateRange?.from ? (
                 dateRange.to ? (
                   <>
-                    {format(dateRange.from, 'LLL dd, y')} -{' '}
-                    {format(dateRange.to, 'LLL dd, y')}
+                    {format(dateRange.from, 'dd-MM-yyyy')} a{' '}
+                    {format(dateRange.to, 'dd-MM-yyyy')}
                   </>
                 ) : (
-                  format(dateRange.from, 'LLL dd, y')
+                  format(dateRange.from, 'dd-MM-yyyy')
                 )
               ) : (
                 <span>{label}</span>
@@ -66,34 +70,51 @@ export const DatePicker = forwardRef<HTMLButtonElement, TDatePicker>(function({ 
             />
           </PopoverContent>
         </Popover>
+        <input
+          className="hidden"
+          required={required}
+          type="date"
+          name={name}
+          value={format(date ?? new Date(), 'yyyy-MM-dd')}
+        />
       </div>
     )
   }
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={'outline'}
-          { ...props }
-          ref={ref}
-          className={cn(
-            'w-full justify-start text-left font-normal',
-            !date && 'text-muted-foreground', props.className
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, 'PPP') : <span>{label}</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+    <>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={'outline'}
+            {...props}
+            ref={ref}
+            className={cn(
+              'w-full justify-start text-left font-normal',
+              !date && 'text-muted-foreground',
+              props.className
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? format(date, 'dd-MM-yyyy') : <span>{label}</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+      <input
+        className="hidden"
+        required={required}
+        type="date"
+        name={name}
+        value={format(date ?? new Date(), 'yyyy-MM-dd')}
+      />
+    </>
   )
 })
