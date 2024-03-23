0f60aaa9cd78c1e0d558bc7dc4f2ec11dbdc9f39
alnavarrop99 <anavarro.develop99@gmail.com>
2024-03-22 21:14:04 -0400

    feat: create breadcrumb component

---
+++ src/components/ui/breadcrumb.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+
+function Basic() {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
+
+const meta: Meta<React.ComponentProps<typeof Breadcrumb>> = {
+  title: '@components/breadcrumb',
+  component: Breadcrumb,
+}
+export default meta
+
+export const Primary: StoryObj<React.ComponentProps<typeof Breadcrumb>> = {
+  args: {},
+  render: Basic,
+}
