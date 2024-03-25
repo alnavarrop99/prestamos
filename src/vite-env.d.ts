b355d8c7e620ac0a7dd4790c5c37e82afa249364
alnavarrop99 <anavarro.develop99@gmail.com>
2024-03-24 20:00:07 -0400

    feat: init msw

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
-declare module '@gcss' {
-  import styles from './styles/gcss.module.css'
-  export default styles
+interface ImportMetaEnv {
+  readonly VITE_API: string
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
