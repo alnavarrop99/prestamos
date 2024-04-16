9e61ea1b9010d633e7758f2b857365d89eaf29ab
alnavarrop99 <anavarro.develop99@gmail.com>
2024-04-16 15:02:29 -0400

    fix: mock current user rol

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -1,147 +1,261 @@
-import type { TUSER_GET } from '@/api/users';
-import type { TCLIENT_GET_BASE } from '@/api/clients';
-import type { TCREDIT_GET, TCUOTE } from '@/api/credit';
-import type { TREPORT_GET } from '@/api/report';
-import { listStatus } from '@/lib/type/status';
-import { listIds } from '@/lib/type/id';
-import { listMoraTypes } from '@/lib/type/moraType';
-import { listFrecuencys } from '@/lib/type/frecuency';
-import { listRols } from '@/lib/type/rol';
-import { formatISO } from 'date-fns';
-import { faker } from "@faker-js/faker/locale/en"
-import { TPAYMENT_GET_BASE } from '@/api/payment';
+import type { TUSER_GET } from '@/api/users'
+import type { TCLIENT_GET_BASE } from '@/api/clients'
+import type { TCREDIT_GET, TCUOTE } from '@/api/credit'
+import type { TREPORT_GET } from '@/api/report'
+import { listStatus } from '@/lib/type/status'
+import { listIds } from '@/lib/type/id'
+import { listMoraTypes } from '@/lib/type/moraType'
+import { listFrecuencys } from '@/lib/type/frecuency'
+import { TROLES, listRols } from '@/lib/type/rol'
+import { formatISO } from 'date-fns'
+import { faker } from '@faker-js/faker/locale/en'
+import { TPAYMENT_GET_BASE } from '@/api/payment'
 
 const CLIENTS_LENGTH = 20
 const USERS_LENGTH = 10
-const CREDITS_LENGTH = 200
+const CREDITS_LENGTH = 20
 const REPORTS_LENGTH = 5
 
-export const users = new Map<number, TUSER_GET>( Array.from({ length: USERS_LENGTH })?.map<[number, TUSER_GET]>( (_, index) => {
-  const id = index + 1
-  return ([id, {
-    id,
-    rol: faker.helpers.arrayElement( listRols()?.map( ({ nombre }) => (nombre) ) ),
-    nombre: faker.person.fullName(),
-    clientes: []
-  }])
-}))
-
-export const clients = new Map<number, TCLIENT_GET_BASE>( Array.from({ length: CLIENTS_LENGTH })?.map<[number, TCLIENT_GET_BASE]>( (_, index) => {
-  const id = index + 1
-  const firstName = faker.person.firstName()
-  const lastName = faker.person.lastName()
-  const userId = faker.number.int({min: 1, max: USERS_LENGTH})
-  return ([id, {
-    id,
-    referencia_id: faker.helpers.maybe( () => faker.number.int( { min: 1, max: CLIENTS_LENGTH } ), { probability: 0.4 } ) ,
-    comentarios: faker.lorem.text(),
-    email: faker.internet.email( { firstName, lastName } ),
-    estado: faker.helpers.arrayElement( listStatus() ).id,
-    celular: faker.phone.number("+33 ###-###-###"),
-    telefono: faker.phone.number("+32 ###-###"),
-    nombres: firstName,
-    apellidos: lastName,
-    direccion: faker.location.streetAddress(),
-    tipo_de_identificacion:  faker.helpers.arrayElement( listIds() )?.id,
-    numero_de_identificacion: faker.string.numeric(10),
-    owner_id: userId,
-  }])
-}))
-
-export const credits = new Map<number, TCREDIT_GET>(  Array.from({ length: CREDITS_LENGTH })?.map<[number, TCREDIT_GET]>( (_, index) => {
-  const id = index + 1
-  const mora = faker.helpers.arrayElement( listMoraTypes() )
-  const moraPorcent = faker.number.float({ min: 1, max: 100, multipleOf: 0.25,  })
-  const moraValue = faker.number.float({ min: 50, max: 200, multipleOf: 5.25,  }) 
-  const frecuency = faker.helpers.arrayElement( listFrecuencys() )
-  const ammount = faker.number.float({ min: 500, max: 5000, multipleOf: 5.25,  })
-  const aprobeDate = faker.date.between({ from: new Date("2020-01-01"), to: new Date() })
-  const aditionalDays = faker.number.int(10)
-  const interest = faker.number.float({ min: 0, max: 30, multipleOf: 0.25,  })
-  const user = faker.helpers.arrayElement( [...users.values()] )
-  const cuotesLength = faker.number.int(12)
-  const paymentsLength = faker.number.int( Math.abs( cuotesLength - faker.number.int(12) ) )
-  const cuotes = new Map<number, TCUOTE>( Array.from({ length: cuotesLength })?.map<[ number, TCUOTE ]>( (_, index) => {
-    const _id = (id + index) + 1
-    const payDate = faker.date.soon({ refDate: aprobeDate, days: index + 1  + index + 1 * faker.number.int(index + 1) })
-    const moraDate = new Date(payDate)
-    moraDate.setDate( payDate.getDate() + aditionalDays )
-    const cuoteAmmount = Math.ceil( ammount / cuotesLength )
-    const payMora = faker.helpers.maybe( () => mora.nombre === "Valor fijo" ? moraValue : cuoteAmmount + cuoteAmmount * moraPorcent/100, { probability: 0.4 } )  
-    const pay = cuoteAmmount + cuoteAmmount * interest/100 + (index < paymentsLength && !!payMora ? payMora : 0)
-    return ([index + 1, {
-      id: index + 1,
-      numero_de_cuota: _id,
-      credito_id: id,
-      fecha_de_pago: formatISO(payDate),
-      valor_de_cuota: cuoteAmmount,
-      valor_pagado: pay,
-      fecha_de_aplicacion_de_mora: formatISO(moraDate),
-      valor_de_mora: !!payMora && index < paymentsLength ? payMora : 0,
-      pagada: index <= paymentsLength  ? true : false,
-  }])}))
-  const payments = new Map<number, TPAYMENT_GET_BASE>( [...cuotes?.values()]?.map<[number, TPAYMENT_GET_BASE]>( ({ id: paymentId, fecha_de_pago, valor_pagado, pagada }) =>{
-    const _id = (id + paymentId) + 1
-    if( !pagada ) return ([paymentId, {
-      id: _id,
-      fecha_de_pago,
-      comentario: "",
-      credito_id: id,
-      valor_del_pago: valor_pagado,
-      registrado_por_usuario_id: user?.id
-    }]);
-    return ([paymentId, {
-      id: _id,
-      fecha_de_pago,
-      comentario: faker.lorem.text(),
-      credito_id: id,
-      valor_del_pago: valor_pagado,
-      registrado_por_usuario_id: user?.id
-    }])
+export const currents = new Map<TROLES, TUSER_GET>(
+  listRols()?.map<[TROLES, TUSER_GET]>(({ nombre }, index) => {
+    const id = index + 1
+    return [
+      nombre,
+      {
+        id,
+        rol: nombre,
+        nombre: faker.person.fullName(),
+        clientes: [],
+      },
+    ]
   })
+)
+
+export const users = new Map<number, TUSER_GET>(
+  Array.from({ length: USERS_LENGTH })?.map<[number, TUSER_GET]>((_, index) => {
+    const id = index + 1
+
+    const list = [...currents.values()]
+    return [
+      id,
+      {
+        id,
+        rol:
+          list?.[index]?.rol ??
+          faker.helpers.arrayElement(listRols()?.map(({ nombre }) => nombre)),
+        nombre: list?.[index]?.nombre ?? faker.person.fullName(),
+        clientes: [],
+      },
+    ]
+  })
+)
+
+export const clients = new Map<number, TCLIENT_GET_BASE>(
+  Array.from({ length: CLIENTS_LENGTH })?.map<[number, TCLIENT_GET_BASE]>(
+    (_, index) => {
+      const id = index + 1
+      const firstName = faker.person.firstName()
+      const lastName = faker.person.lastName()
+      const userId = faker.number.int({ min: 1, max: USERS_LENGTH })
+      return [
+        id,
+        {
+          id,
+          referencia_id: faker.helpers.maybe(
+            () => faker.number.int({ min: 1, max: CLIENTS_LENGTH }),
+            { probability: 0.4 }
+          ),
+          comentarios: faker.lorem.text(),
+          email: faker.internet.email({ firstName, lastName }),
+          estado: faker.helpers.arrayElement(listStatus()).id,
+          celular: faker.phone.number('+33 ###-###-###'),
+          telefono: faker.phone.number('+32 ###-###'),
+          nombres: firstName,
+          apellidos: lastName,
+          direccion: faker.location.streetAddress(),
+          tipo_de_identificacion: faker.helpers.arrayElement(listIds())?.id,
+          numero_de_identificacion: faker.string.numeric(10),
+          owner_id: userId,
+        },
+      ]
+    }
   )
+)
 
-  return ([id, {
-    fecha_de_aprobacion: formatISO(aprobeDate),
-    id,
-    owner_id: faker.helpers.arrayElement( [...clients?.values()] )?.id ?? 1,
-    monto: ammount,
-    estado: faker.helpers?.arrayElement( listStatus() )?.id,
-    comentario: faker.lorem.text(),
-    cobrador_id: faker.helpers.arrayElement( [...users?.values()] )?.id,
-    garante_id: faker.helpers.maybe( () => faker.helpers.arrayElement( [...clients?.values()] )?.id , { probability: 0.6 } ) ?? null,
-    tipo_de_mora_id: mora?.id,
-    valor_de_mora: mora.nombre === "Valor fijo" ? moraValue  : moraPorcent ,
-    tasa_de_interes: interest,
-    dias_adicionales: aditionalDays,
-    numero_de_cuotas: cuotesLength,
-    frecuencia_del_credito_id: frecuency?.id,
-    cuotas: [...cuotes?.values()],
-    tipo_de_mora: mora,
-    frecuencia_del_credito: frecuency,
-    pagos: [...payments?.values()],
-  }])
-}))
+export const credits = new Map<number, TCREDIT_GET>(
+  Array.from({ length: CREDITS_LENGTH })?.map<[number, TCREDIT_GET]>(
+    (_, index) => {
+      const id = index + 1
+      const mora = faker.helpers.arrayElement(listMoraTypes())
+      const moraPorcent = faker.number.float({
+        min: 1,
+        max: 100,
+        multipleOf: 0.25,
+      })
+      const moraValue = faker.number.float({
+        min: 50,
+        max: 200,
+        multipleOf: 5.25,
+      })
+      const frecuency = faker.helpers.arrayElement(listFrecuencys())
+      const ammount = faker.number.float({
+        min: 500,
+        max: 5000,
+        multipleOf: 5.25,
+      })
+      const aprobeDate = faker.date.between({
+        from: new Date('2020-01-01'),
+        to: new Date(),
+      })
+      const aditionalDays = faker.number.int(10)
+      const interest = faker.number.float({ min: 0, max: 30, multipleOf: 0.25 })
+      const user = faker.helpers.arrayElement([...users.values()])
+      const cuotesLength = faker.number.int(12)
+      const paymentsLength = faker.number.int(
+        Math.abs(cuotesLength - faker.number.int(12))
+      )
+      const cuotes = new Map<number, TCUOTE>(
+        Array.from({ length: cuotesLength })?.map<[number, TCUOTE]>(
+          (_, index) => {
+            const _id = id + index + 1
+            const payDate = faker.date.soon({
+              refDate: aprobeDate,
+              days: index + 1 + index + 1 * faker.number.int(index + 1),
+            })
+            const moraDate = new Date(payDate)
+            moraDate.setDate(payDate.getDate() + aditionalDays)
+            const cuoteAmmount = Math.ceil(ammount / cuotesLength)
+            const payMora = faker.helpers.maybe(
+              () =>
+                mora.nombre === 'Valor fijo'
+                  ? moraValue
+                  : cuoteAmmount + (cuoteAmmount * moraPorcent) / 100,
+              { probability: 0.4 }
+            )
+            const pay =
+              cuoteAmmount +
+              (cuoteAmmount * interest) / 100 +
+              (index < paymentsLength && !!payMora ? payMora : 0)
+            return [
+              index + 1,
+              {
+                id: index + 1,
+                numero_de_cuota: _id,
+                credito_id: id,
+                fecha_de_pago: formatISO(payDate),
+                valor_de_cuota: cuoteAmmount,
+                valor_pagado: pay,
+                fecha_de_aplicacion_de_mora: formatISO(moraDate),
+                valor_de_mora:
+                  !!payMora && index < paymentsLength ? payMora : 0,
+                pagada: index <= paymentsLength ? true : false,
+              },
+            ]
+          }
+        )
+      )
+      const payments = new Map<number, TPAYMENT_GET_BASE>(
+        [...cuotes?.values()]?.map<[number, TPAYMENT_GET_BASE]>(
+          ({ id: paymentId, fecha_de_pago, valor_pagado, pagada }) => {
+            const _id = id + paymentId + 1
+            if (!pagada)
+              return [
+                paymentId,
+                {
+                  id: _id,
+                  fecha_de_pago,
+                  comentario: '',
+                  credito_id: id,
+                  valor_del_pago: valor_pagado,
+                  registrado_por_usuario_id: user?.id,
+                },
+              ]
+            return [
+              paymentId,
+              {
+                id: _id,
+                fecha_de_pago,
+                comentario: faker.lorem.text(),
+                credito_id: id,
+                valor_del_pago: valor_pagado,
+                registrado_por_usuario_id: user?.id,
+              },
+            ]
+          }
+        )
+      )
 
+      return [
+        id,
+        {
+          fecha_de_aprobacion: formatISO(aprobeDate),
+          id,
+          owner_id: faker.helpers.arrayElement([...clients?.values()])?.id ?? 1,
+          monto: ammount,
+          estado: faker.helpers?.arrayElement(listStatus())?.id,
+          comentario: faker.lorem.text(),
+          cobrador_id: faker.helpers.arrayElement([...users?.values()])?.id,
+          garante_id:
+            faker.helpers.maybe(
+              () => faker.helpers.arrayElement([...clients?.values()])?.id,
+              { probability: 0.6 }
+            ) ?? null,
+          tipo_de_mora_id: mora?.id,
+          valor_de_mora: mora.nombre === 'Valor fijo' ? moraValue : moraPorcent,
+          tasa_de_interes: interest,
+          dias_adicionales: aditionalDays,
+          numero_de_cuotas: cuotesLength,
+          frecuencia_del_credito_id: frecuency?.id,
+          cuotas: [...cuotes?.values()],
+          tipo_de_mora: mora,
+          frecuencia_del_credito: frecuency,
+          pagos: [...payments?.values()],
+        },
+      ]
+    }
+  )
+)
 
-export const reports = new Map<number, TREPORT_GET>( Array.from({ length: REPORTS_LENGTH })?.map<[number, TREPORT_GET]>( (_, index) => {
-  const id = index + 1
-  return ([id, {
-    id,
-    codigo: faker.hacker.abbreviation(),
-    nombre: faker.hacker.adjective(),
-    comentario: faker.lorem.text(),
-    parametros: Array.from( { length: faker.number.int(4) } )?.map( (_, index) => ({
-      nombre: faker.hacker.verb(),
-      codigo: faker.hacker.abbreviation(),
-      id: index + 1,
-      tipo_dato: faker.helpers.arrayElement([ "number", "str", "date", "like" ]),
-      obligatorio: faker.datatype.boolean(0.7),
-    }))
-  }])
-}))
+export const reports = new Map<number, TREPORT_GET>(
+  Array.from({ length: REPORTS_LENGTH })?.map<[number, TREPORT_GET]>(
+    (_, index) => {
+      const id = index + 1
+      return [
+        id,
+        {
+          id,
+          codigo: faker.hacker.abbreviation(),
+          nombre: faker.hacker.adjective(),
+          comentario: faker.lorem.text(),
+          parametros: Array.from({ length: faker.number.int(4) })?.map(
+            (_, index) => ({
+              nombre: faker.hacker.verb(),
+              codigo: faker.hacker.abbreviation(),
+              id: index + 1,
+              tipo_dato: faker.helpers.arrayElement([
+                'number',
+                'str',
+                'date',
+                'like',
+              ]),
+              obligatorio: faker.datatype.boolean(0.7),
+            })
+          ),
+        },
+      ]
+    }
+  )
+)
 
-const paymentList = [...credits?.values()]?.map<TPAYMENT_GET_BASE[]>( ({ pagos }) => (pagos) ).flat() as TPAYMENT_GET_BASE[]
-export const payments = new Map<number, TPAYMENT_GET_BASE>( paymentList?.map<[number, TPAYMENT_GET_BASE]>( ({ id }, i, list) => ([id ?? i + 1, list?.[i]]) ) )
+const paymentList = [...credits?.values()]
+  ?.map<TPAYMENT_GET_BASE[]>(({ pagos }) => pagos)
+  .flat() as TPAYMENT_GET_BASE[]
+export const payments = new Map<number, TPAYMENT_GET_BASE>(
+  paymentList?.map<[number, TPAYMENT_GET_BASE]>(({ id }, i, list) => [
+    id ?? i + 1,
+    list?.[i],
+  ])
+)
 
-export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTcyNDM4MTMxMn0.p3uaAzSYneGPMwcBbpqIutNGnwMyiyDSBae5TW3X4Es"
+export const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTcyNDM4MTMxMn0.p3uaAzSYneGPMwcBbpqIutNGnwMyiyDSBae5TW3X4Es'
--- src/mocks/user.ts
+++ src/mocks/user.ts
@@ -1,87 +1,139 @@
 import { HttpResponse, http } from 'msw'
-import { TROLES, getRolById } from "@/lib/type/rol"
-import type { TUSER_LOGIN, TUSER_LOGIN_BODY, TUSER_GET, TUSER_POST_BODY } from '@/api/users'
-import { users, token } from '@/mocks/data'
+import { getRolById } from '@/lib/type/rol'
+import type {
+  TUSER_LOGIN,
+  TUSER_LOGIN_BODY,
+  TUSER_GET,
+  TUSER_POST_BODY,
+} from '@/api/users'
+import { users, token, currents as user } from '@/mocks/data'
+import { useToken } from '@/lib/context/login'
 
-export let current = users?.get(1)
+export let current = user?.get(
+  useToken.getState()?.rol?.rolName ?? 'Administrador'
+)
 
-const loginUser = http.post(import.meta.env.VITE_API + '/users/login', async ({ request }) => {
-  const { username, password } =  Object.fromEntries( (await request.formData())?.entries() ) as TUSER_LOGIN_BODY
+const loginUser = http.post(
+  import.meta.env.VITE_API + '/users/login',
+  async ({ request }) => {
+    const { username, password } = Object.fromEntries(
+      (await request.formData())?.entries()
+    ) as TUSER_LOGIN_BODY
 
-  if( !username || !password ) throw new Error("Fail request params")
-  if( ( username !== "admin" && username !== "user" && username !== "cobrador") || password !== "app2002" ) throw new Error("username and password are be incorrect")
+    if (!username || !password) throw new Error('Fail request params')
+    if (
+      (username !== 'admin' &&
+        username !== 'user' &&
+        username !== 'cobrador') ||
+      password !== 'app2002'
+    )
+      throw new Error('username and password are be incorrect')
 
-  if( username === "admin" ) current = [...users?.values()]?.find( ({ rol }) => ( rol === "Administrador" as TROLES ) )
-  else if( username === "user" ) current = [...users?.values()]?.find( ({ rol }) => ( rol === "Usuario" as TROLES ) )
-  else if( username === "cobrador" ) current = [...users?.values()]?.find( ({ rol }) => ( rol === "Cobrador" as TROLES ) )
+    if (username === 'admin') current = user.get('Administrador')
+    else if (username === 'user') current = user.get('Usuario')
+    else if (username === 'cobrador') current = user.get('Cobrador')
 
-  return HttpResponse.json<TUSER_LOGIN>({
-    access_token: token
-  })
-})
-
-const listUsers = http.all(import.meta.env.VITE_API + '/users/list', async ( { request } ) => {
-  const auth = request.headers.get("Authorization")
-  if( !auth || !auth.includes(token) ) throw new Error("not auth")
-  return HttpResponse.json<TUSER_GET[]>( Array.from(users?.values()))
-})
-
-const createUser = http.post( import.meta.env.VITE_API + '/users/create', async ({ request }) => {
-  const auth = request.headers.get("Authorization")
-  if( !auth || !auth.includes(token) ) throw new Error("not auth")
-
-  const newUser = (await request.json()) as TUSER_POST_BODY
-  if( !newUser ) throw new Error("Fail Body request")
-
-  const { rol_id, ...items } = newUser
-  users?.set( users?.size + 1 , { ...items, rol: getRolById({ rolId: rol_id })?.nombre , id: (users.get(users?.size)?.id ?? 0) + 1, clientes: [] } )
-  return HttpResponse.json<TUSER_GET>( users?.get( users?.size ), { status: 201 } )
-} )
-
-const updateUserById = http.patch( import.meta.env.VITE_API + '/users/:usuario_id', async ({params, request }) => {
-  const auth = request.headers.get("Authorization")
-  if( !auth || !auth.includes(token) ) throw new Error("not auth")
-
-  const currentUser = (await request.json()) as TUSER_POST_BODY
-  const { usuario_id } = params as { usuario_id?: string }
-
-  if( !currentUser || !usuario_id ) throw new Error("Fail update request")
-
-  const userId = +usuario_id
-  const user = users?.get(userId)
-  const { rol_id, nombre } = currentUser
-
-  if(!user) throw new Error("User not found")
-  users?.set( userId, { ...user, nombre, rol: getRolById({ rolId: rol_id })?.nombre } )
-
-  return HttpResponse.json<TUSER_GET>( users?.get(userId) )
-})
-
-const userById = http.get( import.meta.env.VITE_API + '/users/by_id/:id_usuario', async ({params, request}) => {
-  const auth = request.headers.get("Authorization")
-  if( !auth || !auth.includes(token) ) throw new Error("not auth")
-
-  const { id_usuario } = params as { id_usuario: string }
-  if( !id_usuario || !users?.has( Number.parseInt(id_usuario) )) {
-    throw new Error("Fail get request")
+    return HttpResponse.json<TUSER_LOGIN>({
+      access_token: token,
+    })
   }
-  const userId = +id_usuario
-  const user = users?.get( userId )
+)
 
-  if(!user) throw new Error("User not found")
+const listUsers = http.all(
+  import.meta.env.VITE_API + '/users/list',
+  async ({ request }) => {
+    const auth = request.headers.get('Authorization')
+    if (!auth || !auth.includes(token)) throw new Error('not auth')
+    return HttpResponse.json<TUSER_GET[]>(Array.from(users?.values()))
+  }
+)
 
-  return HttpResponse.json<TUSER_GET>(user) 
-} )
+const createUser = http.post(
+  import.meta.env.VITE_API + '/users/create',
+  async ({ request }) => {
+    const auth = request.headers.get('Authorization')
+    if (!auth || !auth.includes(token)) throw new Error('not auth')
 
-const currentUser = http.get( import.meta.env.VITE_API + '/users/get_current', async ({ request }) => {
-  const auth = request.headers.get("Authorization")
-  if( !auth || !auth.includes(token) ) throw new Error("not auth")
+    const newUser = (await request.json()) as TUSER_POST_BODY
+    if (!newUser) throw new Error('Fail Body request')
 
-  const user = current
+    const { rol_id, ...items } = newUser
+    users?.set(users?.size + 1, {
+      ...items,
+      rol: getRolById({ rolId: rol_id })?.nombre,
+      id: (users.get(users?.size)?.id ?? 0) + 1,
+      clientes: [],
+    })
+    return HttpResponse.json<TUSER_GET>(users?.get(users?.size), {
+      status: 201,
+    })
+  }
+)
 
-  if(!user) throw new Error("User not found")
+const updateUserById = http.patch(
+  import.meta.env.VITE_API + '/users/:usuario_id',
+  async ({ params, request }) => {
+    const auth = request.headers.get('Authorization')
+    if (!auth || !auth.includes(token)) throw new Error('not auth')
 
-  return HttpResponse.json<TUSER_GET>(user) 
-} )
+    const currentUser = (await request.json()) as TUSER_POST_BODY
+    const { usuario_id } = params as { usuario_id?: string }
 
-export default [listUsers, createUser, updateUserById, userById, currentUser, loginUser]
+    if (!currentUser || !usuario_id) throw new Error('Fail update request')
+
+    const userId = +usuario_id
+    const user = users?.get(userId)
+    const { rol_id, nombre } = currentUser
+
+    if (!user) throw new Error('User not found')
+    users?.set(userId, {
+      ...user,
+      nombre,
+      rol: getRolById({ rolId: rol_id })?.nombre,
+    })
+
+    return HttpResponse.json<TUSER_GET>(users?.get(userId))
+  }
+)
+
+const userById = http.get(
+  import.meta.env.VITE_API + '/users/by_id/:id_usuario',
+  async ({ params, request }) => {
+    const auth = request.headers.get('Authorization')
+    if (!auth || !auth.includes(token)) throw new Error('not auth')
+
+    const { id_usuario } = params as { id_usuario: string }
+    if (!id_usuario || !users?.has(Number.parseInt(id_usuario))) {
+      throw new Error('Fail get request')
+    }
+    const userId = +id_usuario
+    const user = users?.get(userId)
+
+    if (!user) throw new Error('User not found')
+
+    return HttpResponse.json<TUSER_GET>(user)
+  }
+)
+
+const currentUser = http.get(
+  import.meta.env.VITE_API + '/users/get_current',
+  async ({ request }) => {
+    const auth = request.headers.get('Authorization')
+    if (!auth || !auth.includes(token)) throw new Error('not auth')
+
+    const user = current
+
+    if (!user) throw new Error('User not found')
+
+    return HttpResponse.json<TUSER_GET>(user)
+  }
+)
+
+export default [
+  listUsers,
+  createUser,
+  updateUserById,
+  userById,
+  currentUser,
+  loginUser,
+]
