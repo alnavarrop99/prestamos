54167b2220478871b472ed390ea704d7441a96b9
alnavarrop99 <anavarro.develop99@gmail.com>
2024-04-18 14:41:43 -0400

    fix: change date format from '-' to '/' in credit detail page

--- src/pages/_layout/credit_/$creditId.tsx
+++ src/pages/_layout/credit_/$creditId.tsx
@@ -242,7 +242,7 @@ export function CreditById() {
             <li>
               <b>{text.details.date + ':'}</b>{' '}
               <span>
-                {format(new Date(credit?.fecha_de_aprobacion), 'dd-MM-yyyy') +
+                {format(new Date(credit?.fecha_de_aprobacion), 'dd/MM/yyyy') +
                   '.'}
               </span>
             </li>
